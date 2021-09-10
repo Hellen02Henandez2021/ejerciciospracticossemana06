2102ff0cf615c50bef193c4c75351050f0a9b6b4
Hellen02Henandez2021 <87906169+Hellen02Henandez2021@users.noreply.github.com>
2021-09-09 20:22:12 -0600

    segundo commit

---
+++ practicaSemana06/ejercicio2practica.py
@@ -0,0 +1,16 @@
+print("programa que lea temperatura en centigrados")
+TP=int(input("Escribe la temperatura en centigrados"))
+    
+#procedimiento
+if TP <=10:
+    print("Clima es frio")
+
+elif "TP" <10 and "TP" <=20: 
+    print("clima esta nublado")
+
+elif 20<TP<=20:
+    print("clima es caluroso")
+     
+elif TP>30:
+    print("clima es tropical")
+  
