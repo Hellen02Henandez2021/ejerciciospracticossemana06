32e649e588a37ff624cc55bc7d505cfc859c0930
Hellen02Henandez2021 <87906169+Hellen02Henandez2021@users.noreply.github.com>
2021-09-09 20:21:03 -0600

    primer commit

---
+++ practicaSemana06/ejercicio1practica.py
@@ -0,0 +1,10 @@
+ compra=float(input("ingrese el valor de la compra"))
+print("ingrese el dia de la compra")
+obtenerdia=str(input("si la compra fue el dia martes o jueves"))
+
+if obtenerdia =="martes or jueves":
+    descuento=compra *0.15
+else (obtenerdia)= otro dia de la semana:
+      compra=compra
+
+ 
